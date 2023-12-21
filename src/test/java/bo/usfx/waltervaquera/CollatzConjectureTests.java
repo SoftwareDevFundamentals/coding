bbc67ba5f98b2abc480f1971c81698130d9720e0
btt <waltervaquera17@gmail.com>
2023-12-21 19:24:41 -0400

    Added tests classes for each kata solution

---
+++ src/test/java/bo/usfx/waltervaquera/CollatzConjectureTests.java
@@ -0,0 +1,14 @@
+package bo.usfx.waltervaquera;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CollatzConjectureTests {
+    @Test
+    public void Tests() {
+        Assert.assertEquals(CollatzConjecture.hotpo(1), 0);
+        Assert.assertEquals(CollatzConjecture.hotpo(5), 5);
+        Assert.assertEquals(CollatzConjecture.hotpo(6), 8);
+        Assert.assertEquals(CollatzConjecture.hotpo(23), 15);
+    }
+}
