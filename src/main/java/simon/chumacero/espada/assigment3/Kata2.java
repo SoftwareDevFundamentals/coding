74d38a0a0ecbb0dc381a32d53899f74bbe9d08ff
stoic-programmer11 <schumaceroespada@gmail.com>
2023-12-22 17:31:14 -0400

    assigment 3

--- src/main/java/simon/chumacero/espada/assigment3/Kata2.java
+++ src/main/java/simon/chumacero/espada/assigment3/Kata2.java
@@ -2,24 +2,24 @@ package simon.chumacero.espada.assigment3;
 
 public final class Kata2 {
 
-    private Kata2() {
+  private Kata2() {
 
+  }
+
+  public static int hotpo(final int n) {
+    int steps = 0;
+    int currentNumber = n;
+
+    while (currentNumber != 1) {
+      if (currentNumber % 2 == 0) {
+        currentNumber /= 2;
+      } else {
+        currentNumber = 3 * currentNumber + 1;
+      }
+      steps++;
     }
 
-    public static int hotpo(final int n) {
-        int steps = 0;
-        int currentNumber = n;
-
-        while (currentNumber != 1) {
-            if (currentNumber % 2 == 0) {
-                currentNumber /= 2;
-            } else {
-                currentNumber = 3 * currentNumber + 1;
-            }
-            steps++;
-        }
-
-        return steps;
-    }
+    return steps;
+  }
 }
 
