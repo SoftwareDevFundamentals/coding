f98736b94be8272f73c6f16e72b3587e6e7bb67d
Mauricio Flores Garzofino <mauricioflores123321@gmail.com>
2023-12-22 13:59:32 -0400

    First Commit

---
+++ src/main/java/bo/usfx/assignment03/FootballChampionship.java
@@ -0,0 +1,22 @@
+package bo.usfx.assignment03;
+
+public final class FootballChampionship {
+    private FootballChampionship() {
+    }
+    public static int points(final String[] games) {
+        int totalPoints = 0;
+
+        for (String game : games) {
+            int x = Character.getNumericValue(game.charAt(0));
+            int y = Character.getNumericValue(game.charAt(2));
+
+            if (x > y) {
+                totalPoints += 3;
+            } else if (x == y) {
+                totalPoints += 1;
+            }
+        }
+
+        return totalPoints;
+    }
+}
