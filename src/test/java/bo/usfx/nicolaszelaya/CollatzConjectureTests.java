b2b11dbdc31ade135ad764bcfde906fdbe26033a
Nicolás Zelaya <fezelayani@gmail.com>
2023-12-22 07:56:02 -0400

    Tests cases changes to match stylecheck

--- src/test/java/bo/usfx/nicolaszelaya/CollatzConjectureTests.java
+++ src/test/java/bo/usfx/nicolaszelaya/CollatzConjectureTests.java
@@ -5,10 +5,10 @@ import org.testng.annotations.Test;
 
 public class CollatzConjectureTests {
     @Test
-    void sampleTest() {
+    public void tests() {
         Assert.assertEquals(CollatzConjecture.hotpo(1), 0);
         Assert.assertEquals(CollatzConjecture.hotpo(5), 5);
         Assert.assertEquals(CollatzConjecture.hotpo(6), 8);
-        Assert.assertEquals(CollatzConjecture.hotpo(23),15);
+        Assert.assertEquals(CollatzConjecture.hotpo(23), 15);
     }
 }
