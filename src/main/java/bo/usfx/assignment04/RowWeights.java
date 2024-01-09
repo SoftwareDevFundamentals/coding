f956b5d9213992f6c585c2778281a86d46c42614
Mauricio Flores Garzofino <mauricioflores123321@gmail.com>
2024-01-09 15:49:05 -0400

    A04

---
+++ src/main/java/bo/usfx/assignment04/RowWeights.java
@@ -0,0 +1,19 @@
+package bo.usfx.assignment04;
+
+public class RowWeights {
+    private RowWeights() {
+    }
+    public static int[] rowWeights (final int[] weights){
+        int team1Weight = 0;
+        int team2Weight = 0;
+
+        for (int i = 0; i < weights.length; i++) {
+            if (i % 2 == 0) {
+                team1Weight += weights[i];
+            } else {
+                team2Weight += weights[i];
+            }
+        }
+        return new int[] {team1Weight, team2Weight};
+    }
+}
