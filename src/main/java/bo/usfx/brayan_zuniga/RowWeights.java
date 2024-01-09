4319eccb8b9d3ca40cf41f5323159845508992d4
brayan146 <brayan14717@gmail.com>
2024-01-09 15:24:41 -0400

    Brayan_Zuñiga assignment 4

---
+++ src/main/java/bo/usfx/brayan_zuniga/RowWeights.java
@@ -0,0 +1,27 @@
+package bo.usfx.brayan_zuniga;
+
+public class RowWeights {
+    private RowWeights() {
+
+    }
+
+    public static int[] rowWeights(final int[] weights) {
+        int teamOne = 0;
+        int teamTwo = 0;
+
+        if (weights.length == 1) {
+            return new int[]{weights[0], 0};
+        }
+
+        for (int i = 0; i < weights.length; i++) {
+
+            if ((i + 1) % 2 == 0) {
+                teamTwo += weights[i];
+            } else {
+                teamOne += weights[i];
+            }
+        }
+        return new int[]{teamOne, teamTwo};
+    }
+
+}
