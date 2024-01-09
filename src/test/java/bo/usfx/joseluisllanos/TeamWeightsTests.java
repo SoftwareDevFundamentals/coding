9fa6281e47206877570bdd1b2e164537a60f9a26
llanosmjla <llanos.mendoza.joseluisalberto@usfx.bo>
2024-01-09 16:27:16 -0400

    modified TeamWeights 09/01

--- src/test/java/bo/usfx/joseluisllanos/TeamWeightsTests.java
+++ src/test/java/bo/usfx/joseluisllanos/TeamWeightsTests.java
@@ -5,15 +5,16 @@ import org.testng.annotations.Test;
 
 public class TeamWeightsTests {
     @Test
-    public void test1(){
+    public void test1() {
         int[] inputValue = {80};
         int[] expectedValue = {80, 0};
         int[] currentValue = TeamWeights.rowWeights(inputValue);
 
         Assert.assertEquals(currentValue, expectedValue);
     }
+
     @Test
-    public void test2(){
+    public void test2() {
         int[] inputValue = {10, 10, 10, 10};
         int[] expectedValue = {20, 20};
         int[] currentValue = TeamWeights.rowWeights(inputValue);
@@ -22,7 +23,7 @@ public class TeamWeightsTests {
     }
 
     @Test
-    public void test3(){
+    public void test3() {
         int[] inputValue = {10, 20, 30, 40, 50, 60, 70, 80, 90};
         int[] expectedValue = {250, 200};
         int[] currentValue = TeamWeights.rowWeights(inputValue);
