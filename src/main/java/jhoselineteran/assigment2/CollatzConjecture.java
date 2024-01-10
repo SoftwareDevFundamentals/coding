443c54c694443361032ef72aa5594405f5c4b6a2
sjteranc <teran.susanjhoseline@gmail.com>
2024-01-09 22:48:16 -0400

    Assigment 2

--- src/main/java/jhoselineteran/assigment2/CollatzConjecture.java
+++ src/main/java/jhoselineteran/assigment2/CollatzConjecture.java
@@ -1,26 +1,27 @@
 package jhoselineteran.assigment2;
 
+
 public class CollatzConjecture {
-    public static int hotpo (int n) {
-        int steps = 0;
 
-        while (n != 1) {
-            if (n % 2 == 0) {
-                n /= 2;
-            } else {
-                n = 3 * n + 1;
-            }
-            steps++;
-        }
-        return steps;
+  private static int hotpo(int n) {
+    int steps = 0;
+
+    while (n != 1) {
+      if (n % 2 == 0) {
+        n /= 2;
+      } else {
+        n = 3 * n + 1;
+      }
+      steps++;
     }
 
-    public static void main(String[] args) {
-        System.out.println(hotpo(1));   // Output: 0
-        System.out.println(hotpo(5));   // Output: 5
-        System.out.println(hotpo(6));   // Output: 8
-        System.out.println(hotpo(23));  // Output: 15
-    }
+    return steps;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(hotpo(1));
+    System.out.println(hotpo(5));
+    System.out.println(hotpo(6));
+    System.out.println(hotpo(23));
+  }
 }
-
-
