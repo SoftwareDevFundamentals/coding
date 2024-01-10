cfff1a5b2033afaf954588b928b20e9483f3aee5
armandonum <armandonunez404@gmail.com>
2024-01-10 13:53:58 -0400

    assignme_04

---
+++ src/main/java/bo/usfx/armando/Team.java
@@ -0,0 +1,24 @@
+package bo.usfx.armando;
+
+
+
+public final class Team {
+    private Team(){
+
+    }
+
+    public static int[] rowWeights(final int [] weigth ){
+        int counterTeam1=0;
+        int counterTeam2=0;
+        for (int i=0;i<weigth.length;i++) {
+            if ((i % 2) == 0) {
+
+                counterTeam1 += weigth[i];
+            } else {
+                counterTeam2 += weigth[i];
+            }
+        }
+
+        return new int[]{counterTeam1,counterTeam2};
+    }
+}
--- src/test/java/bo/usfx/armandoTest/TeamTEst.java
+++ src/test/java/bo/usfx/armandoTest/TeamTEst.java
@@ -1,11 +1,9 @@
-package bo.usfx.armandonuñez;
-
+package bo.usfx.armandoTest;
+import bo.usfx.armandonuñez.Team;
 import org.testng.Assert;
-
 import org.testng.annotations.Test;
-
-public class TeamTest {
-@Test
+public class TeamTEst {
+    @Test
     public void basicTest() {
         int[] inputValue ={27,34,23,12};
         int[] expectedValue = {50,46};
