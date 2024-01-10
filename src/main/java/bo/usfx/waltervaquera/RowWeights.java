73b1891e8a2ac55852ef649579b5dcb366b4ff19
btt <waltervaquera17@gmail.com>
2024-01-10 12:34:22 -0400

    Changed RowWeigths class because a similar solution alredy existed

--- src/main/java/bo/usfx/waltervaquera/RowWeights.java
+++ src/main/java/bo/usfx/waltervaquera/RowWeights.java
@@ -6,15 +6,10 @@ public final class RowWeights {
     }
 
     public static int[] rowWeights(final int[] weights) {
-        int team1Weight = 0;
-        int team2Weight = 0;
+        int[] totalWeights = new int[2];
         for (int i = 0; i < weights.length; i++) {
-            if (i % 2 == 0) {
-                team1Weight += weights[i];
-            } else {
-                team2Weight += weights[i];
-            }
+            totalWeights[i % 2] += weights[i];
         }
-        return new int[]{team1Weight, team2Weight};
+        return totalWeights;
     }
 }
