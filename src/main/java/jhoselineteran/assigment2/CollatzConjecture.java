a8eba6e95d0b3e7673be1ba4b8bce73d4fc0173e
sjteranc <teran.susanjhoseline@gmail.com>
2024-01-09 23:10:15 -0400

    p1

--- src/main/java/jhoselineteran/assigment2/CollatzConjecture.java
+++ src/main/java/jhoselineteran/assigment2/CollatzConjecture.java
@@ -4,13 +4,18 @@ package jhoselineteran.assigment2;
  * Esta clase implementa la Conjetura de Collatz y proporciona un método para calcular
  * el número de pasos necesarios para llegar a 1 a partir de un número dado.
  */
-public class CollatzConjecture {
+public final class CollatzConjecture {
+
+  private CollatzConjecture() {
+
+  }
   /**
    * Calcula el número de pasos necesarios para llegar a 1 utilizando la Conjetura de Collatz.
    *
    * @param n El número de inicio.
    * @return El número de pasos necesarios.
-   */
+     */
+
   private static int hotpo(int n) {
     int steps = 0;
 
@@ -25,14 +30,13 @@ public class CollatzConjecture {
 
     return steps;
   }
+
   /**
    * Método principal para probar la Conjetura de Collatz con algunos valores de inicio.
    *
-   *
    * @param args Los argumentos de la línea de comandos (no se utilizan en este caso).
    */
-
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     System.out.println(hotpo(1));
     System.out.println(hotpo(5));
     System.out.println(hotpo(6));
--- src/main/java/jhoselineteran/assigment2/TotalPoints.java
+++ src/main/java/jhoselineteran/assigment2/TotalPoints.java
@@ -2,32 +2,35 @@ package jhoselineteran.assigment2;
 
 import java.util.logging.Logger;
 
-public class TotalPoints {
+public final class TotalPoints {
+  private static final Logger LOGGER = Logger.getLogger(TotalPoints.class.getName());
 
-    private static final Logger LOGGER = Logger.getLogger(TotalPoints.class.getName());
+  private TotalPoints() {
+    // Constructor privado para ocultar el constructor predeterminado
+  }
 
-    public static int points(String[] games) {
-        int totalPoints = 0;
+  public static int points(final String[] games) {
+    int totalPoints = 0;
 
-        for (String game : games) {
-            String[] scores = game.split(":");
-            int ourScore = Integer.parseInt(scores[0]);
-            int opponentScore = Integer.parseInt(scores[1]);
+    for (final String game : games) {
+      final String[] scores = game.split(":");
+      final int ourScore = Integer.parseInt(scores[0]);
+      final int opponentScore = Integer.parseInt(scores[1]);
 
-            if (ourScore > opponentScore) {
-                totalPoints += 3;  // Win
-            } else if (ourScore == opponentScore) {
-                totalPoints += 1;  // Tie
-            }
-            // No need to add points for a loss (0 points)
-        }
-
-        return totalPoints;
+      if (ourScore > opponentScore) {
+        totalPoints += 3;  // Win
+      } else if (ourScore == opponentScore) {
+        totalPoints += 1;  // Tie
+      }
+      // No need to add points for a loss (0 points)
     }
 
-    public static void main(String[] args) {
-        String[] games = {"3:1", "2:2", "0:1", "1:3", "4:0", "2:1", "1:1", "3:2", "0:0", "2:2"};
-        int pointsEarned = points(games);
-        LOGGER.info("Our team earned a total of " + pointsEarned + " points.");
-    }
+    return totalPoints;
+  }
+
+  public static void main(final String[] args) {
+    final String[] games = {"3:1", "2:2", "0:1", "1:3", "4:0", "2:1", "1:1", "3:2", "0:0", "2:2"};
+    final int pointsEarned = points(games);
+    LOGGER.info(String.format("Our team earned a total of %d points.", pointsEarned));
+  }
 }
