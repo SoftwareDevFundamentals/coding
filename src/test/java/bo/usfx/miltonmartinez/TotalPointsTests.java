6099e9c9c699752d2745a6fff58f365296fc102c
Milton Angel Martinez Rodriguez <martinez.rodriguez.miltonangel@usfx.bo>
2024-01-12 15:02:42 -0400

    actualizacion 12 enero

---
+++ src/test/java/bo/usfx/miltonmartinez/TotalPointsTests.java
@@ -0,0 +1,20 @@
+package bo.usfx.miltonmartinez;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TotalPointsTests {
+
+    @Test
+    public void basicTests(){
+        Assert.assertEquals(30, TotalPoints.points(new String[] {"1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"}));
+
+        Assert.assertEquals(10, TotalPoints.points(new String[] {"1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"}));
+
+        Assert.assertEquals(0, TotalPoints.points(new String[] {"0:1","0:2","0:3","0:4","1:2","1:3","1:4","2:3","2:4","3:4"}));
+
+        Assert.assertEquals(15, TotalPoints.points(new String[] {"1:0","2:0","3:0","4:0","2:1","1:3","1:4","2:3","2:4","3:4"}));
+
+        Assert.assertEquals(12, TotalPoints.points(new String[] {"1:0","2:0","3:0","4:4","2:2","3:3","1:4","2:3","2:4","3:4"}));
+    }
+}
