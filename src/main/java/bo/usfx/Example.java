36c71c1610126b699a8390c42333f9d53162efb5
stoic-programmer11 <schumaceroespada@gmail.com>
2024-01-12 10:42:31 -0400

    correcciones package

--- src/main/java/bo/usfx/Example.java
+++ src/main/java/bo/usfx/Example.java
@@ -1,18 +1,14 @@
-/**
- * Este paquete contiene la clase Example,que
- * proporciona métodos para realizar operaciones de suma.
- */
 package bo.usfx;
 
 import java.util.stream.IntStream;
 
 public final class Example {
 
-  private Example() {
+    private Example() {
 
-  }
+    }
 
-  public static int summation(final int n) {
-    return IntStream.range(1, n + 1).sum();
-  }
+    public static int summation(final int n) {
+        return IntStream.range(1, n + 1).sum();
+    }
 }
